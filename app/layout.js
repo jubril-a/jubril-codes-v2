5322560ed1d86aa16c545dfa5cfc3c18d02bbc86
Jubril <abdulsalamjubr3@gmail.com>
2025-06-01 09:21:30 +0100

    seo 1

--- app/layout.js
+++ app/layout.js
@@ -2,8 +2,8 @@ import { MonaSansWide } from "./fontloader";
 import "./globals.css";
 
 export const metadata = {
-  title: "Jubril Abdul",
-  description: "Jubril Abdulsalam Porfolio",
+  title: "Jubril Abdulsalam",
+  description: "Jubril Abdul is a frontend developer and tutor at Gatech Integrated Services, building responsive websites with React, Next.js, and Tailwind CSS. Explore his portfolio and projects.",
 };
 
 export default function RootLayout({ children }) {
---
+++ next-sitemap.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    siteUrl: 'https://jubril-codes-v2.vercel.app',
+    generateRobotsTxt: true,
+};
+  
